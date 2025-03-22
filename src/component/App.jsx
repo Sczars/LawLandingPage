5280a0d562e72e6c311ed5618bd591ce550484d5
20127506 <thoang1908@gmail.com>
2025-03-22 21:26:33 +0700

    navigation bar

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,9 +3,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default function App() {
+  const sections = [
+    { id: 1, name: "hero" },
+    { id: 2, name: "reason" },
+    { id: 3, name: "objectives" },
+    { id: 4, name: "services" },
+    { id: 5, name: "testimonial" },
+  ];
+
+  const pageName = "Văn Phòng Luật ABC XYZ"
+
   return (
     <>
-      <Header />
+      <Header sections={sections} pageName={pageName}/>
       <Main />
       <Footer />
     </>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,3 +1,64 @@
-export default function Header(){
-    return <header>This is a Header</header>
+import PropTypes from "prop-types";
+import "../css/header.css";
+import { useState } from "react";
+
+export default function Header(props) {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isSpam, setIsSpam] = useState(false);
+
+  const listItems = props.sections.map((item) => (
+    <li className="nav__item" key={item.id}>
+      <a href={`#section${item.id}`}>{item.name}</a>
+    </li>
+  ));
+
+  const navigation = (
+    <nav className="nav">
+      <ul className="nav__list">{listItems}</ul>
+    </nav>
+  );
+
+  function toggleNav() {
+    if (!isSpam) {
+      setIsSpam(true);
+
+      if (isNavOpen) {
+        const nav = document.querySelector(".nav");
+        nav.classList.add("nav--fade");
+
+        setTimeout(() => {
+          setIsNavOpen((prev) => !prev);
+          setIsSpam(false);
+        }, 500);
+      } else {
+        setIsNavOpen((prev) => !prev);
+        
+        setTimeout(() => {
+          setIsSpam(false);
+        }, 500);
+      }
+    }
+  }
+
+  return (
+    <header className="header">
+      <h1 className="header__logo">{props.pageName}</h1>
+      <div className="dropdown">
+        <button className="header__button button" onClick={toggleNav}>
+          ☰
+        </button>
+        {isNavOpen && navigation}
+      </div>
+    </header>
+  );
 }
+
+Header.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+  pageName: PropTypes.string.isRequired,
+};
